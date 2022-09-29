8b7bab774be8f3bf65d3cd43418c4b28e650915e
Mateus Ito <Mateusito10@gmail.com>
2022-09-29 20:06:52 +0000

    refactor: move createUser mutation to UserResolver

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,6 +1,9 @@
-import { Arg, FieldResolver, Query, Resolver, Root } from 'type-graphql'
+import { UserInputError } from 'apollo-server'
+import { Arg, FieldResolver, Mutation, Query, Resolver, Root } from 'type-graphql'
+import { CreateUserInput } from '../dto/inputs/CreateUserInput'
 import { TodoModel } from '../dto/models/TodoModel'
 import { UserModel } from '../dto/models/UserModel'
+import { User } from '../entities/User'
 import { InMemoryDatabase } from '../repositories/implementations/InMemoryDatabase'
 
 const db = InMemoryDatabase.getInstance()
@@ -13,6 +16,28 @@ export class UserResolver {
     id: string
   ) {
     return await db.getUserById(id)
+  }
+
+  @Mutation(() => UserModel)
+  async createUser(
+    @Arg('data')
+    { email, name }: CreateUserInput
+  ) {
+    const emailAlreadyExists = await db.getUserByEmail(email)
+    if (emailAlreadyExists) {
+      throw new UserInputError('Email already exists', {
+        argumentName: 'email',
+      })
+    }
+
+    const user = new User({
+      email,
+      name,
+    })
+
+    await db.createUser(user)
+
+    return user
   }
 
   @FieldResolver(() => [TodoModel])
--- src/resolvers/UsersResolver.ts
+++ src/resolvers/UsersResolver.ts
@@ -1,8 +1,5 @@
-import { UserInputError } from 'apollo-server'
-import { Arg, Mutation, Query, Resolver } from 'type-graphql'
-import { CreateUserInput } from '../dto/inputs/CreateUserInput'
+import { Query, Resolver } from 'type-graphql'
 import { UserModel } from '../dto/models/UserModel'
-import { User } from '../entities/User'
 import { InMemoryDatabase } from '../repositories/implementations/InMemoryDatabase'
 
 const db = InMemoryDatabase.getInstance()
@@ -12,27 +9,5 @@ export class UsersResolver {
   @Query(() => [UserModel])
   async users() {
     return await db.getAllUsers()
-  }
-
-  @Mutation(() => UserModel)
-  async createUser(
-    @Arg('data')
-    { email, name }: CreateUserInput
-  ) {
-    const emailAlreadyExists = await db.getUserByEmail(email)
-    if (emailAlreadyExists) {
-      throw new UserInputError('Email already exists', {
-        argumentName: 'email',
-      })
-    }
-
-    const user = new User({
-      email,
-      name,
-    })
-
-    await db.createUser(user)
-
-    return user
   }
 }
