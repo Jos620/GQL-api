3b299fc2820a5f69a5198addcd6c0088fd907016
Mateus Ito <Mateusito10@gmail.com>
2022-09-29 16:53:33 +0000

    feat: add

---
+++ src/dto/inputs/CreateUserInput.ts
@@ -0,0 +1,10 @@
+import { Field, InputType } from 'type-graphql'
+
+@InputType()
+export class CreateUserInput {
+  @Field()
+  name: string
+
+  @Field()
+  email: string
+}
--- src/resolvers/UsersResolver.ts
+++ src/resolvers/UsersResolver.ts
@@ -1,9 +1,21 @@
-import { Query, Resolver } from 'type-graphql'
+import { Arg, Mutation, Query, Resolver } from 'type-graphql'
+import { CreateUserInput } from '../dto/inputs/CreateUserInput'
+import { UserModel } from '../dto/models/UserModel'
 
 @Resolver()
 export class UsersResolver {
-  @Query(() => String)
-  async helloWorld() {
-    return 'Hello, World!'
+  @Query(() => [UserModel])
+  async users() {
+    return []
+  }
+
+  @Mutation(() => UserModel)
+  async createUser(@Arg('data') data: CreateUserInput) {
+    const user = {
+      name: data.name,
+      email: data.email,
+    }
+
+    return user
   }
 }
