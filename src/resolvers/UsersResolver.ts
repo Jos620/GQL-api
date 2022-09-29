b646dca0144563198c5dac38fb9334b260a23ace
Mateus Ito <Mateusito10@gmail.com>
2022-09-29 19:32:50 +0000

    chore: improve error handling

--- src/resolvers/UsersResolver.ts
+++ src/resolvers/UsersResolver.ts
@@ -1,3 +1,4 @@
+import { UserInputError } from 'apollo-server'
 import {
   Arg,
   FieldResolver,
@@ -27,7 +28,11 @@ export class UsersResolver {
     { email, name }: CreateUserInput
   ) {
     const emailAlreadyExists = await db.getUserByEmail(email)
-    if (emailAlreadyExists) throw new Error('Email already exists')
+    if (emailAlreadyExists) {
+      throw new UserInputError('Email already exists', {
+        argumentName: 'email',
+      })
+    }
 
     const user = new User({
       email,
