655309c1f04bdc6100de4597b8ebcad5e2dc0029
Mateus Ito <Mateusito10@gmail.com>
2022-09-29 16:31:16 +0000

    chore: auto build schema

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import 'reflect-metadata'
+
 import { ApolloServer } from 'apollo-server'
+import { buildSchema } from 'type-graphql'
+import { UsersResolver } from './resolvers/UsersResolver'
 
 async function main() {
-  const server = new ApolloServer({})
+  const schema = await buildSchema({
+    resolvers: [UsersResolver],
+  })
+
+  const server = new ApolloServer({
+    schema,
+  })
 
   const { url } = await server.listen()
 
